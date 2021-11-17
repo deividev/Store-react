5ed235c3532d081f10df35e113ef395ca690fb45
David Seco Diaz <dsecodi@corp.vodafone.es>
2021-11-17 22:08:09 +0100

    Add toggle dark/ligth mode

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { useDispatch } from 'react-redux';
 import { login, logout } from './redux/actions/user';
 import { GuardLogin } from './routes/GuardLogin'
 
+//Styles
+import { useTheme, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
+
 //Components
 import MenuComponent from './components/container/menu/MenuComponent';
 import LoginComponent from'./components/pure/forms/Login/LoginComponent';
@@ -17,6 +24,8 @@ import ToDo from './pages/to-do/ToDoPage';
 import './App.scss';
 import logo from './logo.svg';
 
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
 
 
 function App() {
@@ -24,28 +33,36 @@ function App() {
   const dispatch = useDispatch();
   const [isLogged, setLogged] = useState(false);
 
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+
+
+
+
+  
 
 
   function setLogin(user) {
-
-    if(!localStorage.getItem(user.email) && !user.isLogin){ 
-      let userLogin = {
-        email: user.email,
-        password: user.password,
-        isLogin: true
-      };
-      localStorage.setItem(user.email, JSON.stringify(userLogin))
-      dispatch(login(userLogin));
-      setLogged(true);
-    } else if (!user.isLogin){
-      let userLogin = {
-        email: user.email,
-        password: user.password,
-        isLogin: true
-      };
-      localStorage.setItem(user.email, JSON.stringify(userLogin))
-      dispatch(login(userLogin));
-      setLogged(true);
+    if (user.valid) {
+      if(!localStorage.getItem(user.email) && !user.isLogin){ 
+        let userLogin = {
+          email: user.email,
+          password: user.password,
+          isLogin: true
+        };
+        localStorage.setItem(user.email, JSON.stringify(userLogin))
+        dispatch(login(userLogin));
+        setLogged(true);
+      } else if (!user.isLogin){
+        let userLogin = {
+          email: user.email,
+          password: user.password,
+          isLogin: true
+        };
+        localStorage.setItem(user.email, JSON.stringify(userLogin))
+        dispatch(login(userLogin));
+        setLogged(true);
+      }
     }
   };
 
@@ -56,31 +73,65 @@ function App() {
   
 
   return (
-    <div className="App">
-      <Router>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>ReactJS | Imagina Formación</h1>
+  
+      <div className="App">
+        <Router>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>ReactJS | Imagina Formación</h1>
+            <button sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+              {theme.palette.mode === 'dark' ? 'dark' : 'ligth'}
+            </button>
+            {isLogged ? <MenuComponent/> : <LoginComponent  send={setLogin}/>}
+            {isLogged ? <button className="clean" onClick={setLogout}>Logout</button> : <span></span>}
+            
+          </header>
+          <div className="App-body">
+            <GuardLogin  path="/todo" component={ToDo}/>
+            <Route  path="/store" component={Store}/>
+            {/* <Route exact path="/register" component={Store}/> */}
 
-          {isLogged ? <MenuComponent/> : <LoginComponent  send={setLogin}/>}
-          {isLogged ? <button className="clean" onClick={setLogout}>Logout</button> : <span></span>}
-          
-        </header>
-        <div className="App-body">
-          <GuardLogin  path="/todo" component={ToDo}/>
-          <Route  path="/store" component={Store}/>
-          {/* <Route exact path="/register" component={Store}/> */}
+            <Route exact path="/about" component={About}/>
+            <Route path="/product/:id" component={CardDetailsProductComponent}/>
 
-          <Route exact path="/about" component={About}/>
-          <Route path="/product/:id" component={CardDetailsProductComponent}/>
+            {isLogged ?  <Redirect to='/todo'/>  : <Redirect to='/login'/>}
+          </div> 
 
-          {isLogged ?  <Redirect to='/todo'/>  : <Redirect to='/login'/>}
-        </div> 
-
-        <footer></footer>
-      </Router> 
-    </div>
+          <footer></footer>
+        </Router> 
+      </div>
+  
   );
 }
 
-export default App;
+
+
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
